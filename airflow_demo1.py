76d72332a0d2002cddbb4edcf7f824f129514f2f
Dan Zaratsian <dtz001@gmail.com>
2018-08-09 16:02:42 -0400

    Update airflow_demo1.py

--- airflow_demo1.py
+++ airflow_demo1.py
@@ -20,20 +20,25 @@ default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime(2018, 8, 2),
+    #'end_date': datetime(2018, 12, 31),
     #'email': ['airflow@example.com'],
     #'email_on_failure': False,
     #'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(seconds=20),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2018, 12, 31),
+    #'queue': 'bash_queue',
+    #'pool': 'backfill',
+    #'priority_weight': 10
 }
 
 
 # Create DAG object (workflow) that runs every 2 minutes.
-dag = DAG('airflow_demo1', default_args=default_args, schedule_interval=timedelta(minutes=2) )
+# https://airflow.apache.org/code.html#airflow.models.DAG
+dag = DAG('airflow_demo1', 
+          default_args=default_args, 
+          schedule_interval=timedelta(minutes=2),
+          concurrency=1
+         )
 
 
 # Task to print date
