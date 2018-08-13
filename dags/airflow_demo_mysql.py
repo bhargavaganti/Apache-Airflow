2c1374ce8bd6a2fa57d59ef78935ca718e8b6d0c
Dan Zaratsian <dtz001@gmail.com>
2018-08-12 21:45:24 -0400

    Update airflow_demo_mysql.py

--- dags/airflow_demo_mysql.py
+++ dags/airflow_demo_mysql.py
@@ -3,7 +3,7 @@
 #
 #   Cloud Composer (Apache Airflow)
 #
-#   Demo of local commands (bash, python) + MySQL Remote SQL
+#   Interact with MySQL
 #
 ###################################################################################
 
@@ -17,70 +17,70 @@ from datetime import datetime, timedelta
 
 
 # Default Arguments
+# These args are applied to each operator
+# https://airflow.apache.org/_modules/airflow/models.html#BaseOperator
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2018, 8, 2),
+    'wait_for_downstream': False,
+    #'start_date': datetime(2018, 8, 2),
+    #'end_date': datetime(2018, 12, 31),
     #'email': ['airflow@example.com'],
     #'email_on_failure': False,
     #'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(seconds=20),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2018, 12, 31),
+    'retries': 2,
+    'retry_delay': timedelta(seconds=180),
+    'retry_exponential_backoff': True,
+    #'queue': 'bash_queue',
+    #'pool': 'backfill',
+    #'priority_weight': 10,
+    'task_concurrency': 1
 }
 
 
 # Create DAG object (workflow) that runs every 2 minutes.
-dag = DAG('airflow_demo_mysql', 
-          default_args=default_args, 
-          schedule_interval=timedelta(minutes=2),
-          concurrency=1
-         )
+# https://airflow.apache.org/code.html#airflow.models.DAG
+# https://airflow.apache.org/_modules/airflow/models.html#DAG
+dag = DAG('airflow_demo_mysql',
+            description='',
+            schedule_interval=timedelta(minutes=5),
+            #start_date=datetime.now().replace(second=0, microsecond=0),
+            start_date=datetime(2018,8,12,19,35),
+            #end_date=datetime(2018,12,31),
+            default_args=default_args,
+            #concurrency=1,
+            default_view='tree',
+            orientation='TB'
+        )
 
 
 # Task to print date
 t1 = BashOperator(
-    task_id='print_date',
+    task_id='start_print_date',
     bash_command='date',
     dag=dag)
 
 
-# Task to sleep for 5 secs.
-t2 = BashOperator(
-    task_id='sleep',
-    bash_command='sleep 5',
-    retries=3,
-    dag=dag)
-
-
-# Python test func, which will be called by Task 3 (t3)
-def simple_py_func():
-    return ('DZ Message executed at ' + str(datetime.now()) )
-
-
-# Task to call python function ("simple_py_func")
-t3 = PythonOperator(
-        task_id='simple_py_func',
-        python_callable=simple_py_func,
-        #provide_context=True,
-        dag=dag)
-
-
-t4 = MySqlOperator(
-        sql="select * from zdatabase.banking",
-        mysql_conn_id="z_mysql_connection1",
+# Task to interact with MySQL
+#
+# MySQL Connection Info:
+#   Conn ID:    mysql_cloudsql
+#   Conn Type:  MySQL
+#   Host:       <ip_address>
+#   Login:      root
+#   Password:   <password>
+#
+t2 = MySqlOperator(
+        task_id='interact_with_mysql',
+        sql='create table ztable100 (cust_id int)',
+        mysql_conn_id='mysql_cloudsql',
+        autocommit=False,
         database="zdatabase",
-        task_id="mysql_query",
         dag=dag)
 
 
 # Create DAG by specifying upstream tasks
 t2.set_upstream(t1)
-t3.set_upstream(t2)
-t4.set_upstream(t2)
 
 
 #ZEND
